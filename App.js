f27e95e95a0a9efcfcd44f65aec5756153bfe5b5
Yusuf Samaila <yusuf7samaila@gmail.com>
2021-12-06 14:36:34 +0100

    mock pages for navigations testing

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home'
+import Notifications from './screens/Notifications'
+import Profile from './screens/Profile'
+import Settings from './screens/Settings'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
   );
 }
 
